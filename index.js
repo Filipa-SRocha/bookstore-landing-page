183f9b154b4bc9d2f9a08362cc686d594a02b38b
Filipa-SRocha <filipa.sr2@gmail.com>
2021-07-17 18:49:04 +0100

    added local storage functionality

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("working");
+console.log('working');
+
+const form = document.getElementById('subscription');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let email = e.target.children[0].value;
+
+	localStorage.setItem(email, email);
+	e.target.children[0].value = '';
+});
