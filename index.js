77d33c70bb004d73446a5ec679fa487da9b0f537
Filipa-SRocha <filipa.sr2@gmail.com>
2021-07-18 01:24:51 +0100

    Added after subscription message

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 console.log('working');
 
 const form = document.getElementById('subscription');
+const formContainer = document.getElementById('subscribe');
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	let email = e.target.children[0].value;
 
 	localStorage.setItem(email, email);
-	e.target.children[0].value = '';
+
+	formContainer.innerHTML = `<p>Vamos enviar o teu código para o email ${email} <br>Boas leituras!</p>`;
 });
